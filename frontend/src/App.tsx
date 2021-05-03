35fdbd170d801f04294c0d1f56d7655b172e631e
Ulisses Willian Reichenbach <uliwill@gmail.com>
2021-05-03 11:58:15 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Semana Spring React DevSuperior</h1>
+      <h1 className="text-primary">Semana Spring React DevSuperior</h1>
     </div>
   );
 }
